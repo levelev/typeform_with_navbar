7986e5d2c44dedb8f8f256350c6a537f22283566
Daniel Levelev <levelev@gmail.com>
2018-03-07 16:13:03 +0100

    added lp para!

--- lp.js
+++ lp.js
@@ -1,8 +1,10 @@
 const url = new URL(document.location);
 const heading1 = document.querySelector('#heading1');
 const heading2 = document.querySelector('#heading2');
+const lpCopy = document.querySelector('#lp-copy');
 const paramsH1 = url.searchParams.get("h1");
 const paramsH2 = url.searchParams.get("h2");
+const paramsLp = url.searchParams.get("lp");
 
 
 if (paramsH1 != null) {
@@ -11,6 +13,11 @@ if (paramsH1 != null) {
 
 if (paramsH2 != null) {
   heading2.innerText = paramsH2;
+}
+
+
+if (paramsLp != null) {
+  lpCopy.innerText = paramsLp;
 }
 
 
