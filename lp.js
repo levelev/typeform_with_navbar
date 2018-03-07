18156d46227113635f87131c4ce161f1e4fb91a4
Daniel Levelev <levelev@gmail.com>
2018-03-07 14:31:45 +0100

    added default h1

--- lp.js
+++ lp.js
@@ -1,5 +1,16 @@
 const url = new URL(document.location);
 const heading1 = document.querySelector('#heading1');
-heading1.innerText = url.searchParams.get("h1");
 const heading2 = document.querySelector('#heading2');
-heading2.innerText = url.searchParams.get("h2");
+const paramsH1 = url.searchParams.get("h1");
+const paramsH2 = url.searchParams.get("h2");
+
+
+if (paramsH1 != null) {
+  heading1.innerText =  paramsH1;
+}
+
+if (paramsH2 != null) {
+  heading2.innerText = paramsH2;
+}
+
+
